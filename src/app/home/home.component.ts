f65aa07c70ccd2544e09d9287127d448a41a02d7
waspryo <wasp2123321@gmail.com>
2019-08-17 18:03:41 +0900

    get a data from API

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from "../data.service";
 
 @Component({
   selector: 'app-home',
@@ -7,15 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  h1Style: boolean = false;
+  users: Object;
 
-  constructor() { }
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getUsers().subscribe(data => {
+      this.users = data
+      console.log(this.users)
+    })
   }
-
-  firstClick() {
-    this.h1Style = true;  
-  }
-
 }
