5bf4f5096fdb5ef0a218ae9428b967b039303f4c
waspryo <wasp2123321@gmail.com>
2019-08-17 17:45:08 +0900

    Event

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  h1Style: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  firstClick() {
+    this.h1Style = true;  
   }
 
 }
